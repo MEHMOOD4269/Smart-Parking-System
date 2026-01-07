68eb995620d2f5c9f71783a6a1480ef89311f6af
chaudhary Mehmood <hijkahijka@gmail.com>
2026-01-07 10:20:18 +0500

    updated parking slot
    Implemented ParkingSlot class

--- ParkingSlot.cpp
+++ ParkingSlot.cpp
@@ -1,1 +1,21 @@
+#include "ParkingSlot.h"
+
+ParkingSlot::ParkingSlot(int id, int zone)
+    : slotId(id), zoneId(zone), isAvailable(true) {}
+
+bool ParkingSlot::getAvailability() const {
+    return isAvailable;
+}
+
+void ParkingSlot::occupy() {
+    isAvailable = false;
+}
+
+void ParkingSlot::release() {
+    isAvailable = true;
+}
+
+int ParkingSlot::getZoneId() const {
+    return zoneId;
+}
 
--- ParkingSlot.h
+++ ParkingSlot.h
@@ -1,1 +1,19 @@
+#ifndef PARKINGSLOT_H
+#define PARKINGSLOT_H
+
+class ParkingSlot {
+private:
+    int slotId;
+    int zoneId;
+    bool isAvailable;
+
+public:
+    ParkingSlot(int id, int zone);
+    bool getAvailability() const;
+    void occupy();
+    void release();
+    int getZoneId() const;
+};
+
+#endif
 
