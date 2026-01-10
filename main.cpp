7aa2f8b54531421f45fef7dc2a43bf25ae2759d4
chaudhary Mehmood <hijkahijka@gmail.com>
2026-01-11 02:33:07 +0500

    Added menu-driven console UI

--- main.cpp
+++ main.cpp
@@ -1,1 +1,15 @@
 
+#include <iostream>
+using namespace std;
+int main() {
+    int choice;
+    do {
+        cout << "\n--- Smart Parking System ---\n";
+        cout << "1. Request Parking\n";
+        cout << "2. Cancel Request\n";
+        cout << "3. Exit\n";
+        cout << "Enter choice: ";
+        cin >> choice;
+    } while (choice != 3);  
+    return 0;
+}
