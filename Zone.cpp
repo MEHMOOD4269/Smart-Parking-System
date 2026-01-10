ee55f6f6825f82f7129f533a614c561565cff97c
chaudhary Mehmood <hijkahijka@gmail.com>
2026-01-11 02:55:53 +0500

    Implemented Zone data structure

--- Zone.cpp
+++ Zone.cpp
@@ -1,1 +1,14 @@
+#include "Zone.h"
+#include "ParkingArea.h"
 
+Zone::Zone(int id, int count) : zoneId(id), areaCount(count) {
+    areas = new ParkingArea[areaCount];
+}
+
+ParkingArea* Zone::getAreas() {
+    return areas;
+}
+
+int Zone::getZoneId() const {
+    return zoneId;
+}
--- Zone.h
+++ Zone.h
@@ -1,1 +1,19 @@
+#ifndef ZONE_H
+#define ZONE_H
+
+#include "ParkingArea.h"
+
+class Zone {
+private:
+    int zoneId;
+    ParkingArea* areas;
+    int areaCount;
+
+public:
+    Zone(int id, int count);
+    ParkingArea* getAreas();
+    int getZoneId() const;
+};
+
+#endif
 
