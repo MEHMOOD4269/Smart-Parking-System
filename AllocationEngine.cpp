390b21c595e2d788d9075f7ccc1adf72bb3e4867
chaudhary Mehmood <hijkahijka@gmail.com>
2026-01-12 03:35:36 +0500

    Fixed AllocationEngine ParkingSlot dependency issues
    Added forward declaration and required includes for AllocationEngine

--- AllocationEngine.cpp
+++ AllocationEngine.cpp
@@ -1,1 +1,20 @@
+#include "AllocationEngine.h"
+#include "ParkingSlot.h"    
+#include "ParkingArea.h"   
 
+ParkingSlot* AllocationEngine::allocate(Zone* zones, int zoneCount, ParkingRequest& request) {
+    
+    // Try preferred zone first
+    for (int i = 0; i < zoneCount; i++) {
+        ParkingArea* areas = zones[i].getAreas();
+        ParkingSlot* slot = areas[0].getAvailableSlot();
+
+        if (slot != nullptr) {
+            slot->occupy();
+            request.changeState(ALLOCATED);
+            return slot;
+        }
+    }
+
+    return nullptr; // no slot found
+}
--- AllocationEngine.h
+++ AllocationEngine.h
@@ -1,1 +1,14 @@
+#ifndef ALLOCATIONENGINE_H
+#define ALLOCATIONENGINE_H
 
+#include "Zone.h"
+#include "ParkingRequest.h"
+
+class ParkingSlot;   
+
+class AllocationEngine {
+public:
+    ParkingSlot* allocate(Zone* zones, int zoneCount, ParkingRequest& request);
+};
+
+#endif
